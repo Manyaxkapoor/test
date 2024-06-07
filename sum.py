d214c6bdeb0425b7c33eb91f0b371c7248e3e3fd
Manyaxkapoor <im.manyakapoor@gmail.com>
2024-06-07 12:50:26 +0530

    made some major changes so as to merge the file

--- sum.py
+++ sum.py
@@ -1,9 +1,16 @@
 def diff(a,b):
     return (a-b)
 
+def add(a,b):
+    return(a+b)
+
 n1=int(input("Enter number 1\n"))
 n2=int(input("Enter number 2\n"))
 
 s=diff(n1,n2)
 print("The difference of two numbers is")
 print(s)
+
+so=add(n1,n2)
+print("The sum of two numbers is")
+print(so)
