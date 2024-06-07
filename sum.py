f7e778a5d7c26fbcd5d4c709b57092436f1f2af6
Manyaxkapoor <im.manyakapoor@gmail.com>
2024-06-07 13:05:44 +0530

    only calculates difference

--- sum.py
+++ sum.py
@@ -1,8 +1,7 @@
 def diff(a,b):
     return (a-b)
 
-def add(a,b):
-    return(a+b)
+
 
 n1=int(input("Enter number 1\n"))
 n2=int(input("Enter number 2\n"))
@@ -11,6 +10,3 @@ s=diff(n1,n2)
 print("The difference of two numbers is")
 print(s)
 
-so=add(n1,n2)
-print("The sum of two numbers is")
-print(so)
