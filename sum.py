e377db7e521d864a0a1ad345162ae56b8ced6d13
Manyaxkapoor <im.manyakapoor@gmail.com>
2024-06-07 12:34:21 +0530

    subtraction

--- sum.py
+++ sum.py
@@ -1,9 +1,9 @@
-def sum(a,b):
-    return (a+b)
+def diff(a,b):
+    return (a-b)
 
 n1=int(input("Enter number 1\n"))
 n2=int(input("Enter number 2\n"))
 
-s=sum(n1,n2)
-print("The sum of two numbers is")
+s=diff(n1,n2)
+print("The difference of two numbers is")
 print(s)
